331386e6fb11afc6e7f21fa86dd8b48ff3807867
Mateus Mourão <mateusm@take.net>
2021-06-22 21:16:01 -0300

    add ex1 resolution

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,13 @@
 // Faça o exercício da equação de GRAVITAÇÃO UNIVERSAL aqui
 // Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+
+const calculateGravitalForce = () => {
+    const constant = parseFloat(document.querySelector("#constante").value);
+    const mass1 = parseFloat(document.querySelector("#massa1").value);
+    const mass2 = parseFloat(document.querySelector("#massa2").value);
+    const distance = parseFloat(document.querySelector("#distancia").value);
+
+    const result = (constant * mass1 * mass2)/Math.pow(distance, 2);
+
+    document.querySelector('#resultado').value = result;
+}
