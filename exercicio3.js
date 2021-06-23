d5edefd68872aa01ce9c16a857ba380c948f664d
Mateus Mourão <mateusm@take.net>
2021-06-22 22:24:04 -0300

    add ex 3 resolution

--- exercicio3.js
+++ exercicio3.js
@@ -1,2 +1,17 @@
 // Faça o exercício dos PARÁGRAFOS aqui
 // Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+
+
+const toggleExpand = (ev) => {
+    const expanded = ev.currentTarget.parentNode.classList.toggle('expandido');
+    ev.currentTarget.innerHTML = expanded ? '-' : '+';
+}
+
+
+const buttons = document.querySelectorAll('.botao-expandir-retrair');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', toggleExpand)
+});
+
+
